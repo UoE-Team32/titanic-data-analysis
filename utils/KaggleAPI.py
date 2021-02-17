3cc5125d71031a0016c2999572476312c206a9f3
batikanaltun <77833353+batikanaltun@users.noreply.github.com>
2021-02-17 23:48:30 +0300

    Update KaggleAPI.py

--- utils/KaggleAPI.py
+++ utils/KaggleAPI.py
@@ -2,4 +2,4 @@ from kaggle.api.kaggle_api_extended import KaggleApi
 
 api = KaggleApi()
 api.authenticate()
-kaggle competitions submit -c titanic -f submission.csv -m "Message"
+api.kaggle competitions submit -c titanic -f submission.csv -m "Message"
