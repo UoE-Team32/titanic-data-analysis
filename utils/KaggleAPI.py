a31842939bc8d021e8a78824c58f286b66590096
batikanaltun <77833353+batikanaltun@users.noreply.github.com>
2021-02-17 23:49:50 +0300

    Update KaggleAPI.py

--- utils/KaggleAPI.py
+++ utils/KaggleAPI.py
@@ -2,4 +2,5 @@ from kaggle.api.kaggle_api_extended import KaggleApi
 
 api = KaggleApi()
 api.authenticate()
+# Save kaggle.json to the file path displayed in the OSError message given when attempting to import kaggle
 api.kaggle competitions submit -c titanic -f submission.csv -m "Message"
